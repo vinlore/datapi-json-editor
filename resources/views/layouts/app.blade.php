e5dce98c51afbdf7091399d1b2e99e1d0df548e8
Vincent Lore <vincent_lore@hotmail.com>
2016-11-22 15:21:06 -0800

    Index and show locations

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,14 +36,15 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+                        {{ config('app.name', 'DataPi') }}
                     </a>
                 </div>
 
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
-                        &nbsp;
+                        <li><a href="{{ url('/locations') }}">Locations</a></li>
+                        <li><a href="{{ url('/languages') }}">Languages</a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
